fbe15563f6f1cb62f5982eb2160c0fa70fbba435
diegocumbe1 <diegocumbre.04@gmail.com>
2026-04-06 23:41:49 -0500

    first commit

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ async function bootstrap() {
 
   const port = Number(process.env.PORT ?? 3001);
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`Server running on http://localhost:${port}`);
   console.log(`Swagger available on http://localhost:${port}/docs`);
